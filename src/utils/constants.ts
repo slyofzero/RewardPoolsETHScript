9f8b81b4df604ff778c78d700cae1816e2dc01e9
slyofzero <sly.of.zero@gmail.com>
2024-09-22 11:28:36 +0530

    Sell at 5%

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const firebaseCollectionPrefix = "_moneta";
-export const sellThreshold = 0.725;
+export const sellThreshold = 0.525;
 export const tokensToNotLiquidate = [
   "0xa00453052A36D43A99Ac1ca145DFe4A952cA33B8",
 ];
